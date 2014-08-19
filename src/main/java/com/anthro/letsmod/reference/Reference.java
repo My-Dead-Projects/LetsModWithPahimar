379c3a98808e3b88c6dca603c24e0791345bf141
anthropomorphic <code@mikefdorst.com>
2014-08-18 19:01:37 -0700

    Add VERSION to Reference

--- src/main/java/com/anthro/letsmod/reference/Reference.java
+++ src/main/java/com/anthro/letsmod/reference/Reference.java
@@ -7,4 +7,5 @@ public class Reference
 {
   public static final String MOD_ID = "LetsMod";
   public static final String MOD_NAME = "Lets Mod With Pahimar";
+  public static final String VERSION = "1.7.2-1.0";
 }
