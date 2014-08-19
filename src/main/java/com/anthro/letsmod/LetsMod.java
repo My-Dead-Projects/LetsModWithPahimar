c8a20e170bbae67803c98c69217f9f739519708a
anthropomorphic <code@mikefdorst.com>
2014-08-18 18:03:09 -0700

    Add initialization event handlers, and an instance

--- src/main/java/com/anthro/letsmod/LetsMod.java
+++ src/main/java/com/anthro/letsmod/LetsMod.java
@@ -1,11 +1,34 @@
 package com.anthro.letsmod;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 /**
  * @author anthropomorphic
  */
 @Mod(modid = "LetsMod", name = "Lets Mod With Pahimar", version = "1.7.2-1.0")
-public class LetsMod {
+public class LetsMod
+{  
+  @Mod.Instance("LetsMod")
+  public static LetsMod instance;
   
+  @Mod.EventHandler
+  public void preInit(FMLPreInitializationEvent e)
+  {
+    
+  }
+  
+  @Mod.EventHandler
+  public void init(FMLInitializationEvent e)
+  {
+    
+  }
+  
+  @Mod.EventHandler
+  public void postInit(FMLPostInitializationEvent e)
+  {
+    
+  }
 }
