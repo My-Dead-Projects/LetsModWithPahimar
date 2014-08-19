376db6cce89e7fe27c69477ae3905173469082cd
anthropomorphic <code@mikefdorst.com>
2014-08-18 22:34:02 -0700

    Add an example config item in ConfigHandler

--- src/main/java/com/anthro/letsmod/config/ConfigHandler.java
+++ src/main/java/com/anthro/letsmod/config/ConfigHandler.java
@@ -1,9 +1,36 @@
 package com.anthro.letsmod.config;
 
+import net.minecraftforge.common.config.Configuration;
+import java.io.File;
+
 /**
  * @author anthropomorphic
  */
 public class ConfigHandler
 {
-  
+  public static void init(File configFile)
+  {
+    Configuration config = new Configuration(configFile);
+    
+    try
+    {
+      config.load();
+      
+      boolean configValue = config.get
+          (
+              Configuration.CATEGORY_GENERAL,
+              "configValue",
+              true,
+              "This is an example config value"
+          ).getBoolean(true);
+    }
+    catch (Exception e)
+    {
+      
+    }
+    finally
+    {
+      config.save();
+    }
+  }
 }
