b2ca19ab954725c2e4a2a2931514aaa76be06302
anthropomorphic <code@mikefdorst.com>
2014-08-18 19:06:31 -0700

    Add (CLIENT/SERVER)_PROXY_CLASS to Reference

--- src/main/java/com/anthro/letsmod/reference/Reference.java
+++ src/main/java/com/anthro/letsmod/reference/Reference.java
@@ -8,4 +8,6 @@ public class Reference
   public static final String MOD_ID = "LetsMod";
   public static final String MOD_NAME = "Lets Mod With Pahimar";
   public static final String VERSION = "1.7.2-1.0";
+  public static final String CLIENT_PROXY_CLASS = "com.anthro.letsmod.proxy.ClientProxy";
+  public static final String SERVER_PROXY_CLASS = "com.anthro.letsmod.proxy.ServerProxy";
 }
