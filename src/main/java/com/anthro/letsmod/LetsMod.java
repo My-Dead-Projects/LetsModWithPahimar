11df70150f5a7c16a21f3757585bf4931ff6478c
anthropomorphic <code@mikefdorst.com>
2014-08-18 18:50:29 -0700

    Add sided proxy to mod class

--- src/main/java/com/anthro/letsmod/LetsMod.java
+++ src/main/java/com/anthro/letsmod/LetsMod.java
@@ -1,6 +1,8 @@
 package com.anthro.letsmod;
 
+import com.anthro.letsmod.proxy.IProxy;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -13,6 +15,13 @@ public class LetsMod
 {  
   @Mod.Instance("LetsMod")
   public static LetsMod instance;
+  
+  @SidedProxy
+  (
+      clientSide = "com.anthro.letsmod.proxy.ClientProxy",
+      serverSide = "com.anthro.letsmod.proxy.ServerProxy"
+  )
+  public static IProxy proxy;
   
   @Mod.EventHandler
   public void preInit(FMLPreInitializationEvent e)
