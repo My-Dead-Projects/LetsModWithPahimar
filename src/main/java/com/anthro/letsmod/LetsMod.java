2b7b90b56ecd97a29154d06051e4484c7dfed0e8
anthropomorphic <code@mikefdorst.com>
2014-08-18 17:46:19 -0700

    Add LetsMod.java

---
+++ src/main/java/com/anthro/letsmod/LetsMod.java
@@ -0,0 +1,11 @@
+package com.anthro.letsmod;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * @author anthropomorphic
+ */
+@Mod(modid = "LetsMod", name = "Lets Mod With Pahimar", version = "1.7.2-1.0")
+public class LetsMod {
+  
+}
