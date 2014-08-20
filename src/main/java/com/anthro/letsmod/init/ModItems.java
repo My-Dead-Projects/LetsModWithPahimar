a4beaee9b8b5049a1e84a50e262930f8335e0dfd
anthropomorphic <code@mikefdorst.com>
2014-08-19 19:20:23 -0700

    Add ItemMapleLeaf

--- src/main/java/com/anthro/letsmod/init/ModItems.java
+++ src/main/java/com/anthro/letsmod/init/ModItems.java
@@ -1,13 +1,20 @@
 package com.anthro.letsmod.init;
 
+import com.anthro.letsmod.item.ItemBase;
+import com.anthro.letsmod.item.ItemMapleLeaf;
+import cpw.mods.fml.common.registry.GameRegistry;
+
 /**
  * @author anthropomorphic
  */
 public class ModItems
 {
   
+  public static final ItemBase mapleLeaf = new ItemMapleLeaf();
+  
   public static void init()
   {
     
+    GameRegistry.registerItem(mapleLeaf, "mapleLeaf");
   }
 }
---
+++ src/main/java/com/anthro/letsmod/item/ItemMapleLeaf.java
@@ -0,0 +1,12 @@
+package com.anthro.letsmod.item;
+
+/**
+ * @author anthropomorphic
+ */
+public class ItemMapleLeaf extends ItemBase
+{
+  public ItemMapleLeaf()
+  {
+    super();
+  }
+}
