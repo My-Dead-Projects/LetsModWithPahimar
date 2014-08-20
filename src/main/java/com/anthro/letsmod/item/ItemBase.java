a887b19fdc619a567f97911d7237a8c87a44af5f
anthropomorphic <code@mikefdorst.com>
2014-08-19 19:15:59 -0700

    Add ItemBase

---
+++ src/main/java/com/anthro/letsmod/item/ItemBase.java
@@ -0,0 +1,14 @@
+package com.anthro.letsmod.item;
+
+import net.minecraft.item.Item;
+
+/**
+ * @author anthropomorphic
+ */
+public class ItemBase extends Item
+{
+  public ItemBase()
+  {
+    super();
+  }
+}
