c214f23eb051d48b5e2c982d7adc664180c0bf6a
anthropomorphic <code@mikefdorst.com>
2014-08-19 19:43:52 -0700

    Override getUnlocalizedName() in ItemBase

--- src/main/java/com/anthro/letsmod/item/ItemBase.java
+++ src/main/java/com/anthro/letsmod/item/ItemBase.java
@@ -1,6 +1,8 @@
 package com.anthro.letsmod.item;
 
+import com.anthro.letsmod.reference.Reference;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 /**
  * @author anthropomorphic
@@ -10,5 +12,30 @@ public class ItemBase extends Item
   public ItemBase()
   {
     super();
+  }
+  
+  @Override
+  public String getUnlocalizedName()
+  {
+    return String.format(
+        "item.%s%s",
+        Reference.MOD_ID.toLowerCase(),
+        getUnwrappedUnlocalizedName(super.getUnlocalizedName())
+    );
+  }
+  
+  @Override
+  public String getUnlocalizedName(ItemStack itemStack)
+  {
+    return String.format(
+        "item.%s%s",
+        Reference.MOD_ID.toLowerCase(),
+        getUnwrappedUnlocalizedName(super.getUnlocalizedName())
+    );
+  }
+  
+  protected String getUnwrappedUnlocalizedName(String unlocalizedName)
+  {
+    return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
   }
 }
