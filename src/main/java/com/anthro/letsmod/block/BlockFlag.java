6b888b5416983f60aaf4dc9a4d0a4f437851d38e
anthropomorphic <code@mikefdorst.com>
2014-08-20 17:09:36 -0700

    Add BlockFlag

---
+++ src/main/java/com/anthro/letsmod/block/BlockFlag.java
@@ -0,0 +1,15 @@
+package com.anthro.letsmod.block;
+
+import net.minecraft.block.material.Material;
+
+/**
+ * @author anthropomorphic
+ */
+public class BlockFlag extends BlockBase
+{
+  protected BlockFlag()
+  {
+    super(Material.rock);
+    setBlockName("flag");
+  }
+}
