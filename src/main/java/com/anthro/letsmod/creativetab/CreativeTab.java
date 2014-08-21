d7aaf4c76cc9bddd0d87b5b042bd7f63d389bf34
anthropomorphic <code@mikefdorst.com>
2014-08-20 18:10:26 -0700

    Add CreativeTab

---
+++ src/main/java/com/anthro/letsmod/creativetab/CreativeTab.java
@@ -0,0 +1,26 @@
+package com.anthro.letsmod.creativetab;
+
+import com.anthro.letsmod.init.ItemRegistry;
+import com.anthro.letsmod.reference.Reference;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+/**
+ * @author anthropomorphic
+ */
+public class CreativeTab
+{
+  public static final CreativeTabs LETS_MOD_TAB = new CreativeTabs(Reference.MOD_ID)
+  {
+    @Override
+    public Item getTabIconItem() {
+      return ItemRegistry.mapleLeaf;
+    }
+    
+    @Override
+  public String getTranslatedTabLabel()
+    {
+      return "Let's Mod";
+    }
+  };
+}
