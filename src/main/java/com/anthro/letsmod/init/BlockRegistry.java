f0443ac0994e2c5591c9e75dc8275d7a56d05856
anthropomorphic <code@mikefdorst.com>
2014-08-20 17:51:03 -0700

    Hold objects in Block/ItemRegistry

--- src/main/java/com/anthro/letsmod/init/BlockRegistry.java
+++ src/main/java/com/anthro/letsmod/init/BlockRegistry.java
@@ -2,11 +2,13 @@ package com.anthro.letsmod.init;
 
 import com.anthro.letsmod.block.BlockBase;
 import com.anthro.letsmod.block.BlockFlag;
+import com.anthro.letsmod.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 /**
  * @author anthropomorphic
  */
+@GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class BlockRegistry
 {
   
--- src/main/java/com/anthro/letsmod/init/ItemRegistry.java
+++ src/main/java/com/anthro/letsmod/init/ItemRegistry.java
@@ -2,11 +2,13 @@ package com.anthro.letsmod.init;
 
 import com.anthro.letsmod.item.ItemBase;
 import com.anthro.letsmod.item.ItemMapleLeaf;
+import com.anthro.letsmod.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 /**
  * @author anthropomorphic
  */
+@GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ItemRegistry
 {
   
