9f5c6710d14297b32a069cb09d6f1f43c26b6616
anthropomorphic <code@mikefdorst.com>
2014-08-22 20:57:11 -0700

    Add KeyInputEventHandler.getPressedKeyBinding()

--- src/main/java/com/anthro/letsmod/client/handler/KeyInputEventHandler.java
+++ src/main/java/com/anthro/letsmod/client/handler/KeyInputEventHandler.java
@@ -1,5 +1,7 @@
 package com.anthro.letsmod.client.handler;
 
+import com.anthro.letsmod.client.settings.KeyBindings;
+import com.anthro.letsmod.reference.Key;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.InputEvent;
 
@@ -8,6 +10,19 @@ import cpw.mods.fml.common.gameevent.InputEvent;
  */
 public class KeyInputEventHandler
 {
+  private static Key getPressedKeyBinding()
+  {
+    if (KeyBindings.charge.isPressed())
+    {
+      return Key.CHARGE;
+    }
+    if (KeyBindings.release.isPressed())
+    {
+      return Key.RELEASE;
+    }
+    return Key.UNKNOWN;
+  }
+  
   @SubscribeEvent
   public void handleKeyInputEvent(InputEvent.KeyInputEvent event)
   {
