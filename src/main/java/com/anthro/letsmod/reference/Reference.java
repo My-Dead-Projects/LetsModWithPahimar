f7857cb2b4614c1f61b1ca06add080d018b2a4ae
anthropomorphic <code@mikefdorst.com>
2014-08-22 20:11:25 -0700

    Lets Mod With Pahimar -> Let's Mod With Pahimar

--- src/main/java/com/anthro/letsmod/reference/Reference.java
+++ src/main/java/com/anthro/letsmod/reference/Reference.java
@@ -6,7 +6,7 @@ package com.anthro.letsmod.reference;
 public final class Reference
 {
   public static final String MOD_ID = "LetsMod";
-  public static final String MOD_NAME = "Lets Mod With Pahimar";
+  public static final String MOD_NAME = "Let's Mod With Pahimar";
   public static final String VERSION = "1.7.10-1.0";
   public static final String CLIENT_PROXY_CLASS = "com.anthro.letsmod.proxy.ClientProxy";
   public static final String SERVER_PROXY_CLASS = "com.anthro.letsmod.proxy.ServerProxy";
